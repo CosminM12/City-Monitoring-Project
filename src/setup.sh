e63d96300c2b1591c6b1e6e7e5a648bda2022f0d
CosminM12 <cosmin.murariu1@gmail.com>
2026-05-25 00:28:20 +0300

    Phase 3 final implementation

--- src/setup.sh
+++ src/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "===1. Compiling programs==="
+echo "=== 1. Compiling Phase 3 Programs ==="
 
 gcc -Wall city_manager.c -o city_manager
 gcc -Wall monitor_reports.c -o monitor_reports
@@ -8,3 +8,58 @@ gcc -Wall scorer.c -o scorer
 gcc -Wall city_hub.c -o city_hub
 
 echo "Compiled successfully"
+
+echo "=== 2. Populating Districts with Data ==="
+# --- DOWNTOWN ---
+./city_manager --role manager --user alice --add downtown <<EOF
+12.5
+21.8
+road
+2
+new pothole reported
+EOF
+
+./city_manager --role manager --user alice --add downtown <<EOF
+13.1
+22.0
+flooding
+3
+major intersection flooded
+EOF
+
+# --- UPTOWN ---
+./city_manager --role manager --user alice --add uptown <<EOF
+10.9
+11.2
+lighting
+1
+broken street light
+EOF
+
+./city_manager --role inspector --user bob --add uptown <<EOF
+11.5
+12.1
+road
+2
+cracked pavement
+EOF
+
+# --- INDUSTRIAL ---
+./city_manager --role inspector --user bob --add industrial <<EOF
+88.8
+99.9
+other
+3
+chemical spill
+EOF
+
+./city_manager --role manager --user charlie --add industrial <<EOF
+89.1
+98.5
+lighting
+1
+flickering security light
+EOF
+
+
+echo "=== Setup Complete! ==="
